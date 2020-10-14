9c93aa6f5e51af91fffa381b122bff8a10f5d217
Dmytro <savchukdsma9@gmail.com>
2020-10-14 14:25:21 +0300

    implement addPerson method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import {StorageService} from '../services/storage.service';
+import {DateCalculationService} from '../services/date-calculation.service';
+
+interface IPerson {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: Date;
+  daysRemaining?: number;
+  age?: number;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'upcoming-birthdays';
+
+  constructor(private storage: StorageService, private dateCalculation: DateCalculationService) {
+    this.persons = this.storage.get<IPerson[]>('persons') || [];
+  }
+
+  errorMessage = '';
+  date = null;
+  firstName = '';
+  lastName = '';
+
+  persons: Array<IPerson> = [];
+
+  addPerson(): void{
+   const today = new Date();
+   if (today.getFullYear() < new Date(this.date).getFullYear()){
+     this.errorMessage = `You could not set year of birthday which is more than ${today.getFullYear()}`;
+   }else {
+     const person: IPerson = {
+       dateOfBirth: this.date,
+       firstName: this.firstName,
+       lastName: this.lastName,
+       daysRemaining: this.dateCalculation.getDaysRemaining(this.date),
+       age: this.dateCalculation.getYears(this.date)
+     };
+
+     this.persons.push(person);
+     this.storage.set<IPerson[]>('persons', this.persons);
+   }
+  }
+
+
 }
