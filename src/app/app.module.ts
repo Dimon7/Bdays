246adf4d3d555d1e7b065913ff155a9a75329e88
Dmytro <savchukdsma9@gmail.com>
2020-10-15 15:09:37 +0300

    imported modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NzTableModule } from 'ng-zorro-antd/table';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -12,20 +12,33 @@ import en from '@angular/common/locales/en';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import {RouterModule} from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
+import {AppRoutingModule} from './app-routing.module';
+import { UpcomingBdayComponent } from './upcoming-bday/upcoming-bday.component';
+import { ManageBdayComponent } from './manage-bday/manage-bday.component';
+import {NzDividerModule} from 'ng-zorro-antd/divider';
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    UpcomingBdayComponent,
+    ManageBdayComponent
   ],
   imports: [
     BrowserModule,
     NzDatePickerModule,
     FormsModule,
     NzLayoutModule,
+    NzTableModule,
     NzButtonModule,
+    AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule,
+    NzDividerModule
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
